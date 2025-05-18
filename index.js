c3761d14ee5015d3c31b406f2a3db4654d81a1b7
jesuslemos <jesuslemosfoncubierta@gmail.com>
2025-05-18 07:47:29 +0200

    feat: añadido bbdd y endpoint ranking

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("./database");
 const app = express();
 const port = 3001;
 
@@ -20,9 +21,41 @@ app.post("/next-move", async (req, res) => {
     const winner = checkWinner(board);
     const gameOver = isGameOver(board);
 
+    if (winner) {
+      try {
+        const [rows] = await db.query(
+          "UPDATE ranking SET player_wins = player_wins + ?, ia_wins = ia_wins + ? WHERE id = 1",
+          [winner === "X" ? 1 : 0, winner === "O" ? 1 : 0]
+        );
+        console.log("Ranking actualizado:", rows);
+      } catch (error) {
+        console.error("Error al actualizar el ranking:", error);
+      }
+    }
+
     res.json({ nextMove: iaMove, winner: winner, gameOver: gameOver });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.get("/ranking", async (req, res) => {
+  try {
+    const [rows] = await db.query(
+      "SELECT player_wins, ia_wins, draws FROM ranking WHERE id = 1"
+    );
+    if (rows.length > 0) {
+      res.json({
+        playerWins: rows[0].player_wins,
+        iaWins: rows[0].ia_wins,
+        draws: rows[0].draws,
+      });
+    } else {
+      res.status(404).json({ error: "Ranking not found" });
+    }
+  } catch (error) {
+    console.error("Error al obtener el ranking:", error);
     res.status(500).json({ error: "Server error" });
   }
 });
