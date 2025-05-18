d20555c48b9fa712cec6ff1a86adcd57351ebd58
jesuslemos <jesuslemosfoncubierta@gmail.com>
2025-05-18 08:15:51 +0200

    fix: actualizacion ranking empate y comprobar si gana ia

--- index.js
+++ index.js
@@ -14,22 +14,44 @@ app.post("/next-move", async (req, res) => {
       return res.status(400).json({ error: "Invalid board" });
     }
 
-    //Para que no se resuelva al instante
-    await new Promise((resolve) => setTimeout(resolve, 750));
+    let winner = checkWinner(board);
+    let gameOver = isGameOver(board);
+    let iaMove = null;
 
-    const iaMove = getNextMove(board, "O");
-    const winner = checkWinner(board);
-    const gameOver = isGameOver(board);
+    //MovimientoIA
+    if (!gameOver && !winner) {
+      //Para que no se resuelva al instante
+      await new Promise((resolve) => setTimeout(resolve, 750));
+      iaMove = getNextMove(board, "O");
+      if (iaMove !== null) {
+        const iaBoard = [...board];
+        iaBoard[iaMove] = "O";
+        winner = checkWinner(iaBoard);
+        gameOver = isGameOver(iaBoard);
+        board[iaMove] = "O";
+      }
+    }
 
+    //Actualizacion del ranking
     if (winner) {
+      console.log("WINNER-> ", winner);
       try {
         const [rows] = await db.query(
-          "UPDATE ranking SET player_wins = player_wins + ?, ia_wins = ia_wins + ? WHERE id = 1",
-          [winner === "X" ? 1 : 0, winner === "O" ? 1 : 0]
+          "UPDATE ranking SET player_wins = player_wins + ?, ia_wins = ia_wins + ?, draws = draws + ? WHERE id = 1",
+          [winner === "X" ? 1 : 0, winner === "O" ? 1 : 0, 0]
         );
-        console.log("Ranking actualizado:", rows);
+        console.log("Ranking actualizado (ganador):", rows);
       } catch (error) {
         console.error("Error al actualizar el ranking:", error);
+      }
+    } else if (gameOver && !winner) {
+      try {
+        const [rows] = await db.query(
+          "UPDATE ranking SET draws = draws + 1 WHERE id = 1"
+        );
+        console.log("Ranking actualizado (empate):", rows);
+      } catch (error) {
+        console.error("Error al actualizar el ranking (empate):", error);
       }
     }
 
